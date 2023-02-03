3093ac6d575ac9fa4acab0feff0d3399ca6227c9
Claudia Iturregui <claudia.iturregui@gmail.com>
2023-02-03 16:15:07 -0500

    completed lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String, required: true, unique: true},
+  level:{
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+  },
+    ingredients: {type: Array},
+    cuisine: {type: String, required: true},
+    dishType: {
+      type: String,
+      enum: ['Breakfast', 'Main', 'Soup', 'Snack', 'Drink', 'Dessert']
+    },
+    image: {
+      type: String,
+      default: 'https://images.media-allrecipes.com/images/75131.jpg'
+    },
+    duation: {type: Number, min: 0},
+    creator: {type: String},
+    created: {type: Date, default: 'today'}
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
